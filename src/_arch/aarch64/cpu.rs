ca1ad4cca775147df311c44a7de2bb75fe409707
Fernando Lugo <lugo.fernando@gmail.com>
2020-08-03 18:16:51 -0700

    create simple rust project
    Initial support for QEMU AARCH64
    
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

---
+++ src/_arch/aarch64/cpu.rs
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2020 Fernando Lugo <lugo.fernando@gmail.com>
+ */
+
+global_asm!(include_str!("loader.S"));
+global_asm!(include_str!("start.S"));
+
+#[no_mangle]
+pub extern "C" fn cpu_start() -> ! {
+    crate::kmain();
+}
