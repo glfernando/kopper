69f621a6750a6b9a7901ef82766bebaca35973d5
Fernando Lugo <lugo.fernando@gmail.com>
2020-10-24 18:57:03 -0700

    build: organize code into 2 crates
    Create library crate for all generic kopper code and 1 binary crate
    which will used that library.
    
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 // Copyright (c) 2020 Fernando Lugo <lugo.fernando@gmail.com>
 //
 
-//! kopper is a simple baremetal kernel
+//! kopper main application
 
 #![no_std]
 #![no_main]
@@ -12,21 +12,14 @@
 #![feature(format_args_nl)]
 #![feature(asm)]
 
-mod cpu;
-mod panic;
-
-/// board specific code
-pub mod board;
-/// devices
-pub mod device;
-/// all kopper libraries
-#[path = "klib.rs"]
-pub mod lib;
+use kopper::println;
 
 static VERSION: &str = "0.0.1";
 
-/// kernel main function
-pub fn kmain() -> ! {
+/// kopper app entry point
+#[no_mangle]
+pub fn main() -> ! {
     println!("Welcome to kopper version {}", VERSION);
+
     loop {}
 }
