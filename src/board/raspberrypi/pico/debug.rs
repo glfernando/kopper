a4355bf5525560f2a6dc2e0d2a8e4c9720f9bbfa
Fernando Lugo <lugo.fernando@gmail.com>
2021-09-20 00:03:52 -0700

    device: update console trait
    Create simple Console Trait. Also move core::fmt::Write trait
    implementation to print library. So, it is not needed for each debug
    module to implement that trait
    
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

--- src/board/raspberrypi/pico/debug.rs
+++ src/board/raspberrypi/pico/debug.rs
@@ -1,20 +1,26 @@
 use crate::device;
-use core::fmt;
-struct QemuDebugCon;
 
-impl device::console::ConsoleOut for QemuDebugCon {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for c in s.chars() {
-            unsafe {
-                while (core::ptr::read_volatile(0x4003_4018 as *mut u32) & (1 << 5)) != 0 {}
-                core::ptr::write_volatile(0x4003_4000 as *mut u32, c as u32);
-            }
+struct DebugCon;
+
+impl device::console::Console for DebugCon {
+    fn putc(&self, c: char) -> Result<(), &'static str> {
+        unsafe {
+            while (core::ptr::read_volatile(0x4003_4018 as *mut u32) & (1 << 5)) != 0 {}
+            core::ptr::write_volatile(0x4003_4000 as *mut u32, c as u32);
         }
         Ok(())
+    }
+
+    fn getc(&self) -> Result<char, &'static str> {
+        let c = unsafe {
+            while (core::ptr::read_volatile(0x4003_4018 as *mut u32) & (1 << 4)) != 0 {}
+            core::ptr::read_volatile(0x4003_4000 as *mut u32) as u8
+        };
+        Ok(c as char)
     }
 }
 
 /// return simple consout out object
-pub fn console() -> impl device::console::ConsoleOut {
-    QemuDebugCon {}
+pub fn console() -> impl device::console::Console {
+    DebugCon {}
 }
--- src/device/console.rs
+++ src/device/console.rs
@@ -1,1 +1,8 @@
-pub use core::fmt::Write as ConsoleOut;
+/// trait for a Console device
+pub trait Console {
+    /// send a char to the console
+    fn putc(&self, c: char) -> Result<(), &'static str>;
+
+    /// get a char from the console
+    fn getc(&self) -> Result<char, &'static str>;
+}
