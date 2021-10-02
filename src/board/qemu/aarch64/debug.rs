fba7cd20e9f3490a8d345aa90a0812758d5c11cf
Fernando Lugo <lugo.fernando@gmail.com>
2021-10-02 00:52:56 -0700

    qemu: aarch64: debug: check UART FIFO status
    Before writing or reading for UART check FIFO status
    
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

--- src/board/qemu/aarch64/debug.rs
+++ src/board/qemu/aarch64/debug.rs
@@ -5,13 +5,17 @@ struct QemuDebugCon;
 impl device::console::Console for QemuDebugCon {
     fn putc(&self, c: char) -> Result<(), &'static str> {
         unsafe {
+            while (core::ptr::read_volatile(0x0900_0018 as *mut u32) & (1 << 5)) != 0 {}
             core::ptr::write_volatile(0x0900_0000 as *mut u32, c as u32);
         }
         Ok(())
     }
 
     fn getc(&self) -> Result<char, &'static str> {
-        let c = unsafe { core::ptr::read_volatile(0x0900_0000 as *mut u32) as u8 };
+        let c = unsafe {
+            while (core::ptr::read_volatile(0x0900_0018 as *mut u32) & (1 << 4)) != 0 {}
+            core::ptr::read_volatile(0x0900_0000 as *mut u32) as u8
+        };
         Ok(c as char)
     }
 }
