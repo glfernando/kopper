ad9d1681867fe2fd4c3289f91eae4ca6842be29c
Fernando Lugo <lugo.fernando@gmail.com>
2022-03-28 23:33:16 -0700

    board: qemu: aarch64: declare PL011 peripheral
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

--- src/board/qemu/aarch64.rs
+++ src/board/qemu/aarch64.rs
@@ -1,8 +1,13 @@
 /// code for debugging
 pub mod debug;
 
+/// peripherals definition
+pub mod peripherals;
+
 /// early qemu-aarch64 initialization
 pub fn early_init() {}
 
 /// qemu-aarch64 initialization
-pub fn init() {}
+pub fn init() {
+    peripherals::init();
+}
---
+++ src/board/qemu/aarch64/peripherals.rs
@@ -0,0 +1,11 @@
+//use crate::device::register_device;
+use crate::device::uart::pl011::Pl011;
+use crate::device::uart::Uart;
+use crate::device::Device;
+
+static UART0: Pl011 = Pl011::new("uart0", 0x0900_0000, 24_000_000, 115200);
+
+/// initialize qemu-aarch64 peripherals
+pub fn init() {
+    UART0.init();
+}
