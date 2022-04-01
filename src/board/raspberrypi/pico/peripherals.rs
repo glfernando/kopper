7feccd02524fbe236667380b90a2f89a620490b3
Fernando Lugo <lugo.fernando@gmail.com>
2022-03-31 23:13:13 -0700

    board: pico: add uart0 definition
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

---
+++ src/board/raspberrypi/pico/peripherals.rs
@@ -0,0 +1,10 @@
+use crate::device::uart::pl011::Pl011;
+use crate::device::uart::Uart;
+use crate::device::Device;
+
+static UART0: Pl011 = Pl011::new("uart0", 0x4003_4000, 125_000_000, 230400);
+
+/// initialize peripherals
+pub fn init() {
+    UART0.init();
+}
